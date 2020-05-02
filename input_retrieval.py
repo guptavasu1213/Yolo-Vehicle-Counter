7da0d8e9dae1bfc5ae8695088e3bfd2a104bb297
Vasu Gupta <guptavasu1213@gmail.com>
2020-05-02 01:17:30 -0600

    Separate input parsing from command line

---
+++ input_retrieval.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+
+# PURPOSE:
+# PARAMETERS:
+# RETURN:
+def parseCommandLineArguments():
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--input", required=True,
+		help="path to input video")
+	ap.add_argument("-o", "--output", required=True,
+		help="path to output video")
+	ap.add_argument("-y", "--yolo", required=True,
+		help="base path to YOLO directory")
+	ap.add_argument("-c", "--confidence", type=float, default=0.5,
+		help="minimum probability to filter weak detections")
+	ap.add_argument("-t", "--threshold", type=float, default=0.3,
+		help="threshold when applying non-maxima suppression")
+
+	args = vars(ap.parse_args())
+
+	# load the COCO class labels our YOLO model was trained on
+	labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+	LABELS = open(labelsPath).read().strip().split("\n")
+	
+	# derive the paths to the YOLO weights and model configuration
+	weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+	configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+	
+	inputVideoPath = args["input"]
+	outputVideoPath = args["output"]
+	confidence = args["confidence"]
+	threshold = args["threshold"]
+
+	return LABELS, weightsPath, configPath, inputVideoPath, outputVideoPath, confidence, threshold 
